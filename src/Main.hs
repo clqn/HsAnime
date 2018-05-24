8bc204ad48c77f2a049b85d5fb747172a35b4738
Kendall Stewart <kstwrt@cs.washington.edu>
2018-05-23 17:23:43 -0700

    gloss example

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,17 @@
 module Main where
 
-import qualified Cell
-import qualified System.Environment as Env
+import Graphics.Gloss as Gl
 
-main = do
-  [boardFileName] <- Env.getArgs
-  boardFile <- readFile boardFileName
-  print boardFile
-  -- print (Cell.fromString boardFile)
+main
+  = Gl.display 
+      (Gl.InWindow
+         "Hello World" -- window title
+         (400, 150)    -- window size
+         (10, 10))     -- window position
+      Gl.white         -- background color
+      picture          -- picture to display
+
+picture
+  = Gl.Translate (-170) (-20) -- shift the text to the middle of the window
+  $ Gl.Scale 0.5 0.5          -- display it half the original size
+  $ Gl.Text "Hello World"     -- text to display
